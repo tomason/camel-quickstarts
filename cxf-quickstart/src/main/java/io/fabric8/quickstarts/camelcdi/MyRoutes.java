8414700b36092fec0ca43422260ffda31851d599
Tomas Schlosser <tschloss@redhat.com>
2015-08-13 17:30:12 +0200

    CXF quickstart.

---
+++ cxf-quickstart/src/main/java/io/fabric8/quickstarts/camelcdi/MyRoutes.java
@@ -0,0 +1,44 @@
+/**
+ *  Copyright 2005-2015 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+package io.fabric8.quickstarts.camelcdi;
+
+import javax.inject.Inject;
+
+import org.apache.camel.Endpoint;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.cdi.ContextName;
+import org.apache.camel.cdi.Uri;
+
+/**
+ * Configures all our Camel routes, components, endpoints and beans
+ */
+@ContextName("myCdiCamelContext")
+public class MyRoutes extends RouteBuilder {
+
+    @Inject
+    @Uri("cxfrs://http://localhost:8080/rest?resourceClasses=io.fabric8.quickstarts.camelcdi.Service&bindingStyle=SimpleConsumer")
+    private Endpoint cxfEndpoint;
+
+    @Override
+    public void configure() throws Exception {
+        // you can configure the route rule with Java DSL here
+
+        from(cxfEndpoint)
+                .log("Request: type='${header.operationName}', body='${body}'")
+                .transform().constant("Hello my friend!");
+    }
+
+}
---
+++ cxf-quickstart/src/main/java/io/fabric8/quickstarts/camelcdi/Service.java
@@ -0,0 +1,17 @@
+package io.fabric8.quickstarts.camelcdi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+@Path("/srv/")
+public class Service {
+
+	@POST
+	@Path("/greet")
+	public Response greet(Person person) {
+		return null;
+	}
+}
